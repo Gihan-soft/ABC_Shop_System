a98ec16616486f26ab7dd68ee6bbabe265e2660e
Gihan-soft <56923047+Gihan-soft@users.noreply.github.com>
2023-02-01 23:32:24 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,12 @@
 <br>
 
 <h4>Please Login To Access The System</h4>
+
+<br>
+<ul>
+<li>username- user@gmail.com</li>
+<li>password- 12345678</li>
+</ul>
 <br>
 
 <img src="{{asset('assets/images/bg1.png')}}" width="800" height="500">
